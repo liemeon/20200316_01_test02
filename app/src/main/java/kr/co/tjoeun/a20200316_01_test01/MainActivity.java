d70e368be8fc0c07aa0069c018ecb8c50ea0331e
liemeon <44192664+liemeon@users.noreply.github.com>
2020-03-17 12:13:41 +0900

    ??????

--- app/src/main/java/kr/co/tjoeun/a20200316_01_test01/MainActivity.java
+++ app/src/main/java/kr/co/tjoeun/a20200316_01_test01/MainActivity.java
@@ -3,12 +3,23 @@ package kr.co.tjoeun.a20200316_01_test01;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    Button loginBtn = null;
+    Button password = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        loginBtn = findViewById(R.id.ioginBtn);
+        password = findViewById(R.id.password);
+
+        loginBtn.setText("회원가입");
+        password.setText("비밀번호를찾아랄");
+
     }
 }
